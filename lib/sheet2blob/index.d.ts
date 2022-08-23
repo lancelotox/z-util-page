ecd15d818e5dde1c933658b0044b967c6a797534
zhoufeng <18226814491@163.com>
2022-08-23 19:37:56 +0800

    声明

--- lib/sheet2blob/index.d.ts
+++ lib/sheet2blob/index.d.ts
@@ -1,6 +1,7 @@
 import type { WorkSheet, WritingOptions } from 'xlsx';
 /**
  * 将XLSX库转化出的sheet数据转化为Blob对象
+ * https://www.npmjs.com/package/xlsx
  * @param sheetList 单个sheet或者sheet数组
  * @param sheetNameList 单个name或者name数组
  * @param options XLSX配置
