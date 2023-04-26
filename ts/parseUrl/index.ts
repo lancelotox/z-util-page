5b1ba1244fd13a7469a09b8ff6e97c80e31cadd6
zhoufeng <18226814491@163.com>
2023-04-26 16:15:54 +0800

    test

--- ts/parseUrl/index.ts
+++ ts/parseUrl/index.ts
@@ -1,6 +1,17 @@
 
-interface URLWithParam extends URL {
-  param: {
+interface URLWithParam {
+  hash: string,
+  host: string,
+  hostname: string,
+  href: string,
+  origin: string,
+  password: string,
+  pathname: string,
+  port: string,
+  protocol: string,
+  search: string,
+  username: string,
+  searchParams: {
     [key: string]: any
   }
 }
@@ -14,18 +25,31 @@ function parseUrl(url: string): URLWithParam | null {
   let Url: URLWithParam | null = null
   const param: any = {}
   try {
-    Url = Object.assign(new URL(url), { param })
+    const temp = new URL(url)
+    Url = {
+      hash: temp.hash,
+      host: temp.host,
+      hostname: temp.hostname,
+      href: temp.href,
+      origin: temp.origin,
+      password: temp.password,
+      pathname: temp.pathname,
+      port: temp.port,
+      protocol: temp.protocol,
+      search: temp.search,
+      username: temp.username,
+      searchParams: param
+    }
   } catch (error) {
     console.log(error)
   }
   if (Url === null) return null
-
+  
   const search: string = Url.search.slice(1)
   const paramList = search.split('&').map(item => item.split('='))
   paramList.forEach(item => {
     param[item[0]] = item[1]
   })
-  
   return Url
 }
 
